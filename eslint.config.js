c258ad06cb08aaced67993b1d74cd5100fbcecc0
OzenFern <ozenfernandes@gmail.com>
2026-08-17 23:52:55 +0530

    Ignore temp directory files

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals';
 
 export default [
     {
-        ignores: ['node_modules/', 'coverage/', 'dist/'],
+        ignores: ['node_modules/', 'coverage/', 'dist/', 'temp/'],
     },
 
     js.configs.recommended,

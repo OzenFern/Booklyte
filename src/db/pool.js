f72041191ddd6fd709174103f399d6d37fb83ea0
OzenFern <ozenfernandes@gmail.com>
2026-08-22 18:50:57 +0530

    Refactor database connection to use environment configuration module

--- src/db/pool.js
+++ src/db/pool.js
@@ -5,7 +5,7 @@
  */
 
 import pg from "pg";
-import "dotenv/config.js";
+import env from "../config/env.js";
 
 const { Pool } = pg;
 
@@ -14,12 +14,6 @@ const { Pool } = pg;
  *
  * @type {Pool}
  */
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const pool = new Pool(env.db);
 
 export default pool;

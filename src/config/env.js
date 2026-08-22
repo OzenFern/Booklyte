1ed40ea634b45c83c9f71d2328a171b2554a2142
OzenFern <ozenfernandes@gmail.com>
2026-08-22 18:51:21 +0530

    Add environment configuration module for application settings

---
+++ src/config/env.js
@@ -0,0 +1,19 @@
+/**
+ * This file contains the environment configuration for the application. It exports an object with various settings that can be used throughout the application. The settings include database connection details, API keys, and other environment-specific variables.
+ * @module env
+ */
+import "dotenv/config.js";
+
+const env = {
+  appPort: process.env.APP_PORT || 3000,
+  sessionSecret: process.env.SESSION_SECRET || "default secret",
+  db: {
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "booklyte",
+    port: process.env.DB_PORT || 5432,
+  }
+};
+
+export default env;

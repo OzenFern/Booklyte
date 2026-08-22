0971c9f64fb6f74e367419861d76041e332f9367
OzenFern <ozenfernandes@gmail.com>
2026-08-22 19:21:08 +0530

    Add central export point for Booklyte routes

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+/**
+ * Central export point for all Booklyte routes.
+ * Provides a convenient way to import all route handlers.
+ *
+ * @module routes
+ */
+
+export bookRoutes from "./bookRoutes.js";
+export libraryRoutes from "./libraryRoutes.js";
+export reviewsRoutes from "./reviewsRoutes.js";

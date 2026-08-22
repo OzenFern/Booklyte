16ff908e91cbd45ac35de99e8b5da705461ace95
OzenFern <ozenfernandes@gmail.com>
2026-08-22 21:36:09 +0530

    Initialize Express application with middleware and routing setup

---
+++ src/app.js
@@ -0,0 +1,72 @@
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import env from "./config/env.js";
+import session from "express-session";
+import flash from "connect-flash";
+import { bookRoutes, reviewsRoutes, libraryRoutes } from "./routes/index.js";
+import { notFoundHandler, errorHandler, cacheMiddleware } from "./middlewares/index.js";
+
+const app = express();
+
+// Setup __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Setup EJS template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware
+
+// Sets up sessions
+app.use(
+  session({
+    secret: env.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+// Sets up flash messages
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+
+  next();
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+// TODO: Uncomment this middleware after completing the public folder setup
+// Cache public folder
+// app.use(
+//     express.static(path.join(__dirname, "public"), {
+//         maxAge: "30d",
+//     }),
+// );
+
+// Logging HTTP requests using morgan
+app.use(morgan("dev"));
+// TODO: Add helmet middleware for security
+// TODO: Add compression middleware for response compression
+// TODO: Add cache middleware for caching static assets
+
+// TODO: Add routes for the application
+app.use("/books", bookRoutes);
+app.use("/library/books", reviewsRoutes);
+app.use("/library", libraryRoutes);
+
+// 404 Not Found handler - must be after all routes
+app.use(notFoundHandler);
+
+// Global error handler - must be last
+app.use(errorHandler);
+
+app.listen(env.appPort, () => {
+  console.log(`Booklyte running on http://localhost:${env.appPort}`);
+});
